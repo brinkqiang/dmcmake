4163bb304a8192a327cc523f7f4a5b9c8f468c8e
brinkqiang <brink.qiang@gmail.com>
2019-03-19 10:30:48 +0800

    add

---
+++ cmake/ModuleImport.cmake
@@ -0,0 +1,16 @@
+
+function(ModuleImport ModuleName ModulePath)
+    LINK_DIRECTORIES(${CMAKE_SOURCE_DIR}/lib)
+    SET(EXECUTABLE_OUTPUT_PATH ${CMAKE_SOURCE_DIR}/bin)
+    SET(LIBRARY_OUTPUT_PATH ${CMAKE_SOURCE_DIR}/lib)  
+    ADD_SUBDIRECTORY(Path)
+
+    IF (WIN32)
+        INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/src/windows)
+    ENDIF(WIN32)
+
+    INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/src)
+    INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath})
+    INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/include/${ModuleName})
+    INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/include)
+endfunction(ModuleImport)
