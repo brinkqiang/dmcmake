d292743dfd2a3b464c5bcbb485c1931ff04ed8a1
brinkqiang <brink.qiang@gmail.com>
2022-01-22 18:04:08 +0800

    add

--- build.bat
+++ build.bat
@@ -3,17 +3,9 @@ rmdir /S /Q build
 mkdir build
 pushd build
 
-rem setlocal ENABLEDELAYEDEXPANSION
-set core_str="NumberOfCores"
-
-for /F %%j in ( 'wmic cpu get /value^|findstr %core_str%' ) do ( set core_count=%%j )
-
-echo core_count=%core_count:~14,2%
-
-set buildstr=-- /maxcpucount:%core_count:~14,2%
-
 cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
-cmake --build . --config relwithdebinfo %buildstr%
+cmake --build . --config relwithdebinfo -- /m:%NUMBER_OF_PROCESSORS%
+
 popd
 
 rem pause
